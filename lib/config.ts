6e655a502b7152464568d8a95860c8ef79e089bd
Adithya M <122851882+Thunder5912@users.noreply.github.com>
2026-07-25 14:28:25 +0530

    Add Google API credentials to config

--- lib/config.ts
+++ lib/config.ts
@@ -1,3 +1,9 @@
 export const BOT_TOKEN = process.env.BOT_TOKEN!;
 export const OMDB_API_KEY = process.env.OMDB_API_KEY!;
 export const SHEET_ID = process.env.SHEET_ID!;
+
+export const GOOGLE_CLIENT_EMAIL =
+  process.env.GOOGLE_CLIENT_EMAIL!;
+
+export const GOOGLE_PRIVATE_KEY =
+  process.env.GOOGLE_PRIVATE_KEY!.replace(/\\n/g, "\n");
