faf086f9a1aa70a542529900ebe27315c34ed6eb
Adithya M <122851882+Thunder5912@users.noreply.github.com>
2026-07-25 14:28:56 +0530

    Update sheets.ts

--- lib/sheets.ts
+++ lib/sheets.ts
@@ -1,1 +1,29 @@
-export {};
+import { google } from "googleapis";
+import {
+  GOOGLE_CLIENT_EMAIL,
+  GOOGLE_PRIVATE_KEY,
+  SHEET_ID,
+} from "./config";
+
+const auth = new google.auth.JWT(
+  GOOGLE_CLIENT_EMAIL,
+  undefined,
+  GOOGLE_PRIVATE_KEY,
+  ["https://www.googleapis.com/auth/spreadsheets"]
+);
+
+const sheets = google.sheets({
+  version: "v4",
+  auth,
+});
+
+export async function getMovies() {
+  const response = await sheets.spreadsheets.values.get({
+    spreadsheetId: SHEET_ID,
+    range: "Movies!A:H",
+  });
+
+  const rows = response.data.values || [];
+
+  return rows;
+}
